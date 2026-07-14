2b66f775100fbdec1120b6c1a9aa0a370755acb1
Danila Ganchar <danila.ganchar@gmail.com>
2026-07-14 11:54:54 +0200

    #28 from_expr() hints

--- david8_clickhouse/protocols/sql.py
+++ david8_clickhouse/protocols/sql.py
@@ -1,17 +1,32 @@
+from typing import Union
+
 from david8.protocols.sql import ExprProtocol, FunctionProtocol, QueryProtocol
 from david8.protocols.sql import InsertProtocol as _InsertProtocol
 from david8.protocols.sql import SelectProtocol as _SelectProtocol
 
 
+class TableFunctionProtocol(ExprProtocol): ...
+
 class SelectProtocol(_SelectProtocol):
-    def from_table(self, table_name: str, alias: str = '', db_name: str = '', final: bool = False) -> 'SelectProtocol':
-        """
-        final flag: https://clickhouse.com/docs/sql-reference/statements/select/from#final-modifier
-        """
+    def from_table(
+        self,
+        table_name: str,
+        alias: str = '',
+        db_name: str = '',
+        final: bool = False
+    ) -> 'SelectProtocol': ...
 
     def sample(self, value: int | float, offset: int | float = None) -> 'SelectProtocol': ...
 
+    def from_expr(
+        self,
+        expr:
+        Union['SelectProtocol', FunctionProtocol, TableFunctionProtocol],
+        alias: str = '',
+    ) -> 'SelectProtocol': ...
+
     def limit_by(self, *args: str, limit: int = None, offset: int = None) -> 'SelectProtocol': ...
+
 
 class CreateTableProtocol(QueryProtocol):
     def engine(self, value: str) -> 'CreateTableProtocol': ...
@@ -25,12 +40,5 @@ class CreateTableProtocol(QueryProtocol):
     def on_cluster(self, name: str) -> 'CreateTableProtocol': ...
 
 
-class TableFunctionProtocol(ExprProtocol): ...
-
-
 class InsertProtocol(_InsertProtocol):
-    def into_table_fn(self, fn: TableFunctionProtocol) -> 'InsertProtocol':
-        """
-        Insert into table function:
-        https://clickhouse.com/docs/sql-reference/table-functions
-        """
+    def into_table_fn(self, fn: TableFunctionProtocol) -> 'InsertProtocol': ...
